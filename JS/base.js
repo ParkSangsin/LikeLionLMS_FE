c79086d2c17363b33f7bcf836fca83cc94684a99
ParkSangsin <pss6039@naver.com>
2024-04-05 01:57:02 +0900

    주석 추가

--- JS/base.js
+++ JS/base.js
@@ -1,20 +1,26 @@
 function LoadNAVBAR()  {
+    // 현재 문서의 모든 요소 가져오기
     const allElements = document.getElementsByTagName('*');
 
+    // 모든 요소에 대해 반복
     for(var i = 0; i < allElements.length; i++) {
-        var el = allElements[i];
+        var el = allElements[i]; // 현재 요소
 
+        // 현재 요소의 "nav-include-path" 속성 가져오기
         var navpath = el.getAttribute("nav-include-path");
 
+        // 속성이 있으면 실행
         if (navpath) {
-            var xhttp = new XMLHttpRequest();
-            xhttp.onreadystatechange = function () {
+            var xhttp = new XMLHttpRequest(); // 멤버 함수 동적 할당
+            xhttp.onreadystatechange = function () { // 서버 응답 변경될 때마다 실행 (콜백 함수)
                 if (this.readyState == 4 && this.status == 200) {
                     el.innerHTML = this.responseText;
                     el.removeAttribute("nav-include-path");
-                    LoadNAVBAR();
+                    LoadNAVBAR(); // Event Handler -> 재귀
                 }
             }
+
+            // 서버로 http 요청
             xhttp.open("GET", navpath, true);
             xhttp.send();
             return;
